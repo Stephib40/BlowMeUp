f2600fb047f9f76a8630e584f533ef8727fc73df
Steph Banks <Stephibanks4.0@gmail.com>
2017-07-14 16:18:11 -0400

    Add files via upload

---
+++ DisplayBoard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlowMeUp
+{
+	class DisplayBoard //will allow up to a 5x5 minefield. 
+	{//build method for the
+		
+		
+		DisplayBoard() { }
+		public void Display(string[,] builtArray){
+		//pass the array that is full of zeros and pass it the indexs
+		for (int i = 0; i<builtArray.GetLength(0);i++)
+            {
+                Console.Write("_");
+			
+			for (int j = 0; j<builtArray.GetLength(1);j++)
+				{
+					Console.Write("|" + builtArray[i,j]);
+				}
+			}
+		}
+	}	
+}
---
+++ DrawBoard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlowMeUp//build the array board for "model"
+{
+	class DrawBoard//drawing out the array of 1,0's
+	{
+		static void Print2DArray(int[,] arr)
+		{
+		
+			for (int i = 0; i < arr.GetLength(0); i++)
+			{
+				for (int j = 0; j < arr.GetLength(1); j++)
+				{
+					System.Console.WriteLine("Element({0},{1})={2}", i, j, arr[i, j]);
+				}
+			}
+		}
+
+		static void BuildArray()
+		{
+			Print2DArray(new int[,] { { 0, 0, 0, 0, 0 } , { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0 } });
+		}
+		
+	
+
+
+	}
+
+}
